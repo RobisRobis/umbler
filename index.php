73c31ef31233c0841f7763f2a7bad8beec2841aa
Robis <gabrielrobis26@gmail.com>
2019-11-28 17:29:41 -0300

    link do css

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <title>Teste de Auto Deploy</title>
 </head>
 <body>
-    <div class="title center">
-        <h1>Blaack Hawk</h1>
-    </div>
-    <div class="hawkIcon center">
-        <img class="hawkIcon" src="img/hawk.png" alt="hawkImage">
-    </div>
+    <section>
+        <div class="title center">
+            <h1>Blaack Hawk</h1>
+        </div>
+        <div class="hawkIcon center">
+            <img class="hawkIcon" src="img/hawk.png" alt="hawkImage">
+        </div>
+    </section>
 </body>
 </html>
 
